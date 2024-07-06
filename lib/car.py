acf2fb4fdf5261d8b1eecbb35461264f008f010b
EMMANUEL <waruterewachira7@gmail.com>
2024-07-06 15:23:48 +0300

    complete

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,14 @@
 from vehicle import Vehicle
 
 class Car(Vehicle):
-    pass
+
+    def __init__(self, wheel_size, wheel_number):
+        super().__init__(wheel_size, wheel_number)
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+
+    def fill_up_tank(self):
+        return "filling up!"
